6743eea6f1b8533543defcae5ee50d8ab80a9149
unknown <soleil-2005@mail.ru>
2021-06-03 22:57:03 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
 
 
 class App extends React.Component {
