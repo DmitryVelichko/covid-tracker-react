1fb6ccef1bbeb68c424effe08b103e651eb1e00c
unknown <soleil-2005@mail.ru>
2021-06-09 22:04:53 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import React from 'react';
 // import Chart from './components/Chart/Chart';
 // import CountryPicker from './components/CountryPicker/CountryPicker';
 
-import { Cards, Chart, CountyPicker } from './components';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards />
+                <CountryPicker />
+                <Chart />               
             </div>
         )
     }
 }
 
 export default App;
+
 
