0a34e8469a62961e82330a9aa92e3444f03d82db
unknown <soleil-2005@mail.ru>
2021-06-09 22:07:03 +0300

    New commit

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-const fetchData = async () => {
+export const fetchData = async () => {
     try {
       const response = await axios.get(url); 
-
+      return response;
     
     } catch (error) {
 
