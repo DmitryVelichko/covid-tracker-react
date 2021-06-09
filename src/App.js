ba334722203378a156af59f91d38551c69ca309f
unknown <soleil-2005@mail.ru>
2021-06-09 22:08:32 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
+import { fetchData } from './api';
 
 
 class App extends React.Component {
