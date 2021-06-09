d28904330c99dfa272059a1c5ec3c8872c6326ad
unknown <soleil-2005@mail.ru>
2021-06-09 23:56:46 +0300

    New commit

--- src/api/index.js
+++ src/api/index.js
@@ -4,9 +4,10 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try {
-      const data = await axios.get(url); 
-      console.log(data);
+        
+      const { data: { confirmed, recovered, deaths, lastUpdate } }  = await axios.get(url); 
       
+      return { confirmed, recovered, deaths, lastUpdate };
     
     } catch (error) {
 
