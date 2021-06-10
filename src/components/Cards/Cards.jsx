f8f6117703a3ed050e0e502d08b77a1841278c6c
unknown <soleil-2005@mail.ru>
2021-06-10 23:41:42 +0300

    New commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 const Cards = () => {
     return (
