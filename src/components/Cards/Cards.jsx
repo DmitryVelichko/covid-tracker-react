9cb09ea2671bc5ce10eb991d60cbfbaa2c2330da
unknown <soleil-2005@mail.ru>
2021-06-11 13:45:10 +0300

    New commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,6 +11,7 @@ const Cards = () => {
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">REAL DATA</Typography>
                         <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
