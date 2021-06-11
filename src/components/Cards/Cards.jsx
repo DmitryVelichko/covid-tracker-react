9ff6d8244d2a97bb57fcf3b42478fcd534d5bcef
unknown <soleil-2005@mail.ru>
2021-06-11 23:49:13 +0300

    New commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
 import styles from './Cards.module.css'
 
-const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
     if(!confirmed) {
-        return 'Loading...'
+        return 'Loading...';
     }
     return (
         <div className={styles.container}>
