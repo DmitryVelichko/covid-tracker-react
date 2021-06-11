dd961b6a6a4b1aa63b49ab2e31de79c4adce9e27
unknown <soleil-2005@mail.ru>
2021-06-11 23:50:14 +0300

    New commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core';
-import styles from './Cards.module.css'
+import CountUp from 'react-countup';
+import styles from './Cards.module.css';
 
 const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
     if(!confirmed) {
