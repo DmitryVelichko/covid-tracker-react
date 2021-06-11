a7180dd7551293b6569f31c56889b7701256033b
unknown <soleil-2005@mail.ru>
2021-06-11 23:47:49 +0300

    New commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -3,13 +3,16 @@ import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-u
 import styles from './Cards.module.css'
 
 const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
+    if(!confirmed) {
+        return 'Loading...'
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify='center'>
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
                         <Typography color="textSecondary">REAL DATE</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
