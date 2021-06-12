14dbf831092a76178c32dd0f21eb17fdc85baa61
unknown <soleil-2005@mail.ru>
2021-06-12 15:47:30 +0300

    New commit

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -14,14 +14,9 @@ const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate} }) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
-                            <CountUp 
-                                start={0}
-                                end={confirmed.value}
-                                duration={5}
-                                separator=","
-                            />
+                            <CountUp start={0} end={confirmed.value} duration={5} separator=","/>
                         </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
+                        <Typography color="textSecondary">{lastUpdate}</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
