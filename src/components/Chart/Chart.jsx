fab6c7bb7e2bf67e0c0a422f452634db0d7bd739
unknown <soleil-2005@mail.ru>
2021-06-14 14:43:01 +0300

    New commit

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
 
 const Chart = () => {
     const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    })
     return (
         <h1>Chart</h1>
     )
