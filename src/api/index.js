bde582393c4748a474a2f77f1c9876e21f303bee
unknown <soleil-2005@mail.ru>
2021-06-14 14:33:02 +0300

    New commit

--- src/api/index.js
+++ src/api/index.js
@@ -16,7 +16,7 @@ export const fetchData = async () => {
 
 export const fetchDailyDate = async () => {
   try {
-    const response = await axios.get(`${url}/daily`);
+    const { data } = await axios.get(`${url}/daily`);
   } catch (error) {
 
   }
