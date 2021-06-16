f9e7c03197af85100a8a1d807e6891e3d6654038
unknown <soleil-2005@mail.ru>
2021-06-16 23:21:06 +0300

    New commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
         return (
             <div className={styles.container}>
                 <Cards data={data} />
-                <CountryPicker />
+                <CountryPicker hadnleCountryChange={this.hadnleCountryChange} />
                 <Chart />               
             </div>
         )
